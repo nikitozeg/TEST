9d16f1c08ddb16ad79e5d90aaf95b2e80373b34c
nikitozeg <tazg@ya.ru>
2013-10-09 23:10:25 +0400

    Version 1.1
    Here is a simply memory form that saves a value

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,8 +9,14 @@ import models.*;
 
 public class Application extends Controller {
 
-    public static void index() {
-    
+	// статическая переменная, которая будет хранить наше значение
+    public static String savedValue = "";
+	public static void index(boolean isPost, String value) {
+		if (isPost){
+			savedValue=value;
+			redirect("Application.index");
+		}
+		renderArgs.put("myVar", savedValue);
         render();
     }
 
