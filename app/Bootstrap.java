1aa6554c6e8b9923e39f66b9524756ec1759ea9c
nikitozeg <tazg@ya.ru>
2013-11-28 19:23:06 +0400

    Remove obvious comment

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -8,7 +8,6 @@ import models.*;
 public class Bootstrap extends Job {
  
     public void doJob() {
-        // Check if the database is empty
         if(User.count() == 0) {
             Fixtures.loadModels("initial-data.yml");
         }
