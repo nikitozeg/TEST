43cb2d4e2733a95c887d6b7a7da3212853c28abd
nikitozeg <tazg@ya.ru>
2013-12-02 20:40:07 +0400

    Remove authentication(one more try:)

--- app/controllers/Admin.java
+++ app/controllers/Admin.java
@@ -14,7 +14,7 @@ public class Admin extends Controller {
     static void setConnectedUser() {
         if(Security.isConnected()) {
             User user = User.find("byEmail", Security.connected()).first();
-            renderArgs.put("user", user.fullname);
+            return true;
         }
     }
  
